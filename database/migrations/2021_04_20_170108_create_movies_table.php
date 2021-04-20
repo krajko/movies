0a8ce7500bb85d9e2e44061e0eab27578cfdd935
krajko <nikolakrajinovic99@gmail.com>
2021-04-20 19:08:51 +0200

    zadatak 2

---
+++ database/migrations/2021_04_20_170108_create_movies_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMoviesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('movies', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            
+            $table->string('title')->unique();
+            $table->string('genre');
+            $table->string('director');
+            $table->date('year');
+            $table->text('storyline');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('movies');
+    }
+}
